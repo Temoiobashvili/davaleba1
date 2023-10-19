143c7773c47dd865d6919fcb9deb2272d8c559dd
Temoiobashvili <144713900+Temoiobashvili@users.noreply.github.com>
2023-10-20 01:29:51 +0400

    davaleba1 js

---
+++ script.js
@@ -0,0 +1,18 @@
+let number = 100;
+
+if (number < 50) {
+    console.log("<50");
+}else if (number > 20) {
+    console.log(">20");
+} else {
+    console.log("error");
+}
+
+
+let userName = "mariam";
+
+let result = (userName == "mariam") ? "true" : "false";
+
+console.log(result);
+
+
